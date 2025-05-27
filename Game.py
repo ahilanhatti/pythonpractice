3cf6b12ae0e8a867d49c2fb4b21dec774b868cfb
Ahilan Hatti <hattiahilan@gmail.com>
2025-05-27 11:17:41 -0700

    basic physics with one block

--- Game.py
+++ Game.py
@@ -1,23 +1,46 @@
 import pygame
 def update_physics ():
-    global acc_x, acc_y, vel_x, vel_y, x, y
+    global acc_x, acc_y, vel_x, vel_y, x, y, state
     vel_x += acc_x
     if vel_x > maxvel_x:
         vel_x = maxvel_x
     if vel_x < -maxvel_x:
         vel_x = -maxvel_x
+    if state == 'on ground' and vel_x != 0.0:
+        if vel_x < 0:
+            vel_x += 1.0
+        else:
+            vel_x -= 1.0
     vel_y += acc_y
     if vel_y > maxvel_y:
         vel_y = maxvel_y
     if vel_y < -maxvel_y:
         vel_y = -maxvel_y
     x += vel_x
+    rect = pygame.Rect(x,y,30,30)
+    while block.colliderect(rect) == True:
+        if vel_x > 0:
+            x-=1.0
+        else:
+            x+=1.0
+        rect = pygame.Rect(x,y,30,30)
     y += vel_y
+    rect = pygame.Rect(x,y,30,30)
+    while block.colliderect(rect) == True:
+        if vel_y > 0:
+            y-=1.0
+            state = 'on ground'
+        else:
+            y+=1.0
+        rect = pygame.Rect(x,y,30,30)
     if y>470:
+        state = 'on ground'
         y=470.0
 pygame.init ()
 screen = pygame.display.set_mode([800,600])
+block = pygame.Rect(500,460,80,40)
 running=True
+state = 'in air'
 x=100.0
 y=100.0
 vel_x = 0.0
@@ -25,9 +48,10 @@ vel_y = 0.0
 acc_x = 0.0
 acc_y = 10.0
 maxvel_x = 15.0
-maxvel_y = 70.0
+maxvel_y = 50.0
 while running==True:
     screen.fill((192,192,192))
+    pygame.draw.rect(screen, (0,0,255), block)
     rect = pygame.Rect(x,y,30,30)
     pygame.draw.rect(screen,(255,0,0),rect)
     ground=pygame.Rect(0,500,800,13)
@@ -41,8 +65,9 @@ while running==True:
         acc_x=2.0
     else:
         acc_x = 0.0
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and state != 'in air':
         vel_y=-50.0
+        state = 'in air'
     if keys[pygame.K_s]:
         y+=1.0
     update_physics ()
